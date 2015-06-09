7f8c039d8cd8185b3cd2705daf5d82187c2dcf26
Rodrigue Villetard <rodrigue@oblady.fr>
2015-06-09 13:27:23 +0200

    reject on image onerror

--- angular-images-tools.js
+++ angular-images-tools.js
@@ -50,6 +50,10 @@ angular.module('angular-images-tools', []).service('imageTools', ['$q', function
             
             var resultImg = new Image();
 
+            img.onerror = function(e) {
+                def.reject(e);
+            };
+
             img.onload = function() {
 
                 var canvas = document.createElement('canvas');
